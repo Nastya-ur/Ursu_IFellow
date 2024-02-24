cbe6027f3baf5107fde26b7fed74fbe9930c7b68
Урсу Анастасия <a-ursu20@yandex.ru>
2024-02-25 00:46:24 +0300

    init commit

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RunTest {
+    import org.junit.jupiter.api.AfterAll;
+    import org.junit.jupiter.api.BeforeAll;
+    import org.junit.jupiter.api.Test;
+    import org.junit.jupiter.api.Assertions;
+
+    public class SimpleTest {
+
+        @BeforeAll
+        public static void setUp() {
+            System.out.println("Вывод до всех тестов");
+        }
+
+        @Test
+        public void testAddition() {
+            int a = 5;
+            int b = 10;
+            assertEquals(15, a + b, "5 + 10 должно быть равно 15");
+        }
+
+        @Test
+        public void testSubtraction() {
+            int a = 10;
+            int b = 5;
+            assertEquals(5, a - b, "10 - 5 должно быть равно 5");
+        }
+
+        @AfterAll
+        public static void tearDown() {
+            System.out.println("Вывод после всех тестов");
+        }
+    }
+}
+
+
